85cdca30c8a315fca655b33ee28e89e54dae33ed
TamiraAtieno1 <tamiraatieno20@gmail.com>
2023-07-29 13:01:50 +0300

    created Recipe App

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react'
+import React from 'react'
+import './App.css'
+import SearchBar from './components/SearchBar'
+import RecipeCard from './components/RecipeCard'
+import Pagination from './components/Pagination'
+const searchApi = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+
+function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [info, results] = recipes;
+  
+  // search for the recipe
+  const searchRecipes = async () => {
+    setIsLoading(true);
+    const url = searchApi + query
+    const res = await fetch(url);
+    const data = await res.json();
+    //console.log(data)
+    setRecipes(data.meals);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    searchRecipes()
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchRecipes();
+  }
+
+  return (
+    <>
+      <div className="container">
+      <h2>Our Tasty Recipes!</h2>
+      <SearchBar
+        isLoading={isLoading}
+        query={query}
+        setQuery={setQuery}
+        handleSubmit={handleSubmit}
+      />
+      <div className="recipes">
+        
+        {recipes ? recipes.map(recipe => (
+          <RecipeCard
+             key={recipe.idMeal}
+             recipe={recipe}
+          />
+        )) : "No Results."}
+      </div>
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        updatePageNumber={updatePageNumber}
+      />
+      </div>
+    </>
+  )
+}
+
+export default App;
