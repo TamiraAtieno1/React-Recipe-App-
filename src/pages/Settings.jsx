5788a30d6f3ab28cb8443a985890c22145cf6951
TamiraAtieno1 <tamiraatieno20@gmail.com>
2023-08-11 08:45:14 +0300

    updated responsive design

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,10 +1,82 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Settings = () =>{
+    const [settings, setSettings] = useState({
+        "--background-color": "#fff",
+        "--background-light": "#fff",
+        "--shadow-color": "rgba(0,0,0,0.2)",
+        "--primary-color": "rgba(255, 0, 86)",
+        "--text-color": "#0A0A0A ",
+        "--text-light": "#575757",
+        "--font-size": "16px",
+        "--animation-speed": 1
+    })
+    useEffect(() => {
+        const root = document.documentElement
+        for(let key in settings){
+            root.style.setProperty(key, settings[key])
+        }
+    }, [settings])
+
     const [theme, setTheme] = useState("light")
+    const themes = [
+        {
+            "--background-color": "#fff",
+            "--background-light": "#fff",
+            "--shadow-color": "rgba(0,0,0,0.2)",
+            "--text-color": "#0A0A0A ",
+            "--text-light": "#575757"
+        },
+        {
+            "--background-color": "rgba(29, 29, 29)",
+            "--background-light": "rgba(77, 77, 77)",
+            "--shadow-color": "rgba(0,0,0,0.2)",
+            "--text-color": "#ffffff ",
+            "--text-light": "#eceaea",
+        }
+    ]
+
+    function changeTheme(i){
+        const _theme = {...themes[i]}
+        setTheme(i === 0 ? "light" : "dark")
+        // update settings
+        let _settings = {...settings}
+        for(let key in _theme){
+            _settings[key] = _theme[key]
+        }
+        setSettings(_settings)
+    }
+
+    function changeColor(i){
+        const _color = primaryColors[i]
+        let _settings = {...settings}
+        _settings["--primary-color"] = _color
+        setPrimaryColor(i)
+        setSettings(_settings)
+    }
+
+    function changeFontSize(i){
+        const _size = fontSizes[i]
+        let _settings = {...settings}
+        _settings["--font-size"] = _size.value
+        setFontSize(i)
+        setSettings(_settings)
+    }
+
+    function changeAnimationSpeed(i){
+        const _speed = animationSpeeds[i]
+        let _settings = {...settings}
+        _settings["--animation-speed"] = _speed.value
+        setAnimationSpeed(i)
+        setSettings(_settings)
+    }
+
+
+
+
     const primaryColors = [
         "rgba(255, 0, 86)",
         "rgba(33, 150, 24)",
@@ -48,14 +120,14 @@ const Settings = () =>{
             <div className="section d-block">
                 <h2>Preffered theme</h2>
                 <div className="options-container">
-                    <div className="option light">
+                    <div className="option light" onClick={() => changeTheme(0)}>
                         { theme === "light" && (
                             <div className="check"> 
                               <FontAwesomeIcon icon={faCheck}/>
                             </div>
                         )}
                     </div>
-                      <div className="option dark">
+                      <div className="option dark" onClick={() => changeTheme(1)}>
                         { theme === "dark" && (
                             <div className="check"> 
                               <FontAwesomeIcon icon={faCheck}/>
@@ -68,7 +140,7 @@ const Settings = () =>{
                 <h2>Primary color</h2>
                 <div className="options-container">
                     { primaryColors.map((color, index)=> (
-                        <div className="option dark" style={{backgroundColor: color}}>
+                        <div className="option dark" style={{backgroundColor: color}} onClick={()=> changeColor(index)}>
                         { primaryColor === index && (
                             <div className="check"> 
                               <FontAwesomeIcon icon={faCheck}/>
@@ -82,7 +154,7 @@ const Settings = () =>{
                 <h2>Font Size</h2>
                 <div className="options-container">
                     { fontSizes.map((size, index)=> (
-                    <button className="btn">
+                    <button className="btn" onClick={()=> changeFontSize(index)}>
                         {size.title}
                         { fontSize === index && <span><FontAwesomeIcon icon={faCheck}/></span>}
                     </button>
@@ -93,7 +165,7 @@ const Settings = () =>{
                 <h2>Animation Speed</h2>
                 <div className="options-container">
                     { animationSpeeds.map((speed, index)=> (
-                    <button className="btn">
+                    <button className="btn" onClick={()=> changeAnimationSpeed(index)}>
                         {speed.title}
                         { animationSpeed === index && <span><FontAwesomeIcon icon={faCheck}/></span>}
                     </button>
